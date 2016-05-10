f8222087ed3c6c96b98fdf5445cb91226957d910
Steve Witman <stevewitman@gmail.com>
2016-05-10 16:24:26 -0600

    access data submitted from the form

--- src/script.js
+++ src/script.js
@@ -3,4 +3,7 @@ var module = angular.module('main', []);
 module.controller('formCtrl', ['$scope', function($scope) {
   $scope.data = {};
   $scope.disabled = true;
+  $scope.submit = function() {
+    alert('Form Submitted\nEmail: ' + $scope.data.email + '\nPassword: ' + $scope.data.password)
+  }
 }]);
